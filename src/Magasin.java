aaf375f5b340931e2af599da903a1395c3d68f5e
Gauthier <gauthier.mauche@gmail.com>
2022-11-03 09:55:59 +0100

    add Magasin

---
+++ src/Magasin.java
@@ -0,0 +1,17 @@
+public class Magasin {
+
+    // Attributs
+    private ArrayList<Product> products;
+    private ArrayList<Client> clients;
+    private ArrayList<Commande> commandes;
+
+    // Constructeur
+    public Magasin() {
+        this.products = new ArrayList<Product>();
+        this.clients = new ArrayList<Client>();
+        this.commandes = new ArrayList<Commande>();
+    }
+
+
+
+}
